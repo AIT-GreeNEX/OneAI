6e81791640e2d41c1d8b91feb77cd5e9924f0ab8
zannatul225 <zannatulferdous489@gmail.com>
2022-02-17 16:22:32 +0600

    navbar

--- static/scripts.js
+++ static/scripts.js
@@ -17,3 +17,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
 })
 
+    /* Set the width of the side navigation to 250px */
+    function openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+    }
+
+    /* Set the width of the side navigation to 0 */
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+    }
