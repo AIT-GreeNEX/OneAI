2569e9c1025b66b12279bee262a8b7d147fd7036
MD ABIR HASAN MUNNA <18511091@student.bup.edu.bd>
2022-02-25 06:26:06 +0600

    fixed importing issue

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask  
 from flask import render_template
 from flaskwebgui import FlaskUI
-from handlers import file_loader
+from handlers.file_loader import load_csv
 
 app = Flask(__name__)
-ui = FlaskUI(app, width=500, height=500) 
+ui = FlaskUI(app, width=500, height=500)
 
 
 @app.route("/")
@@ -12,8 +12,10 @@ def hello():
     return render_template('index.html')
 
 @app.route("/home", methods=['GET'])
-def home(): 
-    return render_template('some_page.html')
+def home():
+    res = load_csv("test")
+    print(res)
+    return render_template('some_page.html', test = res)
 
 
 
